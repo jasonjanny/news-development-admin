db68ac17aed58cbb97a4794d974079305cea6f5b
janny <2431349655@qq.com>
2020-12-03 15:47:17 +0800

    实现登录页

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,62 @@
 <template>
-  <div>登录页</div>
+  <el-row type="flex" justify="center" align="middle" class="container">
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="用户名">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码">
+        <el-input v-model="form.password" type="password"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登陆</el-button>
+        <el-button @click="reset">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </el-row>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      this.$axios({
+        method: "post",
+        url: "/login",
+        data: this.form,
+      }).then((res) => {
+        // console.log(res);
+        if (res.data.message === "登录成功") {
+          this.$message.success(res.data.message);
+        } else {
+            this.$message.fail(res.data.message);
+        }
+
+        localStorage.setItem("token", res.data.data.token);
+      });
+    },
+    reset() {
+      this.form = {
+        username: "",
+        password: "",
+      };
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  height: 100vh;
+}
 </style>
