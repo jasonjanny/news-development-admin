b53f5ebd200a1e364695b327ba8d3d7a3517972c
janny <2431349655@qq.com>
2020-12-03 11:25:04 +0800

    引入axios和element-ui库

--- src/main.js
+++ src/main.js
@@ -4,6 +4,22 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import axios from 'axios'
+
+// 绑定到原型
+Vue.prototype.$axios = axios;
+
+// 引入 ui 库
+import ElementUI from 'element-ui';
+// 引入 css
+import 'element-ui/lib/theme-chalk/index.css';
+//注册组件库
+Vue.use(ElementUI);
+
+//设置默认的 api 域名基准路径
+// axios.defaults.baseURL = 'http://127.0.0.1:3000'
+axios.defaults.baseURL = 'http://liangwei.tech:3000'
+
 new Vue({
   router,
   render: function (h) { return h(App) }
