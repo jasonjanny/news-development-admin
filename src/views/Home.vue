1ffe9b73f75bfb1b37dc709d4e666c496e26d30a
janny <2431349655@qq.com>
2020-12-03 16:35:47 +0800

    实现侧边栏菜单管理

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="home">hello</div>
+  <el-container class="container">
+    <el-aside width="200px">
+      <h5>头条管理</h5>
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        background-color="#555"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -7,4 +31,47 @@ export default {};
 </script>
 
 <style lang="less">
+.container {
+  height: 100vh;
+}
+.el-menu {
+  width: 100%;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #333;
+  color: #333;
+  text-align: center;
+  line-height: 100px;
+  h5 {
+    color: #fff;
+    font-size: 20px;
+  }
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
 </style>
