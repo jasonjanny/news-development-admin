67859b6537530ec4e5d9440fc71e8fb24ddc13e9
janny <2431349655@qq.com>
2020-12-04 16:23:52 +0800

    使用图片上传组件

--- src/views/ReleasePost.vue
+++ src/views/ReleasePost.vue
@@ -1,10 +1,50 @@
 <template>
-<div>发布文章</div>
+  <div class="container">
+    <div class="nav">首页 / 文章列表</div>
+
+    <el-upload
+      :action="$axios.defaults.baseURL + '/upload'"
+      list-type="picture-card"
+      :headers="{
+        Authorization: token,
+      }"
+      :on-success="uploadSuccess"
+      :on-remove="removeSuccess"
+    >
+      <i class="el-icon-plus"></i>
+    </el-upload>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      token: localStorage.getItem("token"),
+    };
+  },
+
+  methods: {
+    //  图片上传成功
+    uploadSuccess(res, file) {
+      console.log(res);
+    },
+
+    // 图片移除成功
+    removeSuccess(file) {
+      console.log("图片移除成功");
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  .nav {
+    height: 40px;
+    line-height: 40px;
+    margin-bottom: 40px;
+    text-align: left;
+  }
+}
 </style>
